7a860983becdf03ce5df3e575fef8ba7020d1df4
Prota <protez95@gmail.com>
2020-02-05 00:22:55 +0100

    ad baner

---
+++ Assets/Scripts/Monetization/BannerAdScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class BannerAdScript : MonoBehaviour
+{
+
+    public string gameId = "3360460";
+    public string placementId = "bannerPlacement";
+    public bool testMode = true;
+
+    void Start()
+    {
+        Advertisement.Initialize(gameId, testMode);
+        StartCoroutine(ShowBannerWhenReady());
+    }
+
+    IEnumerator ShowBannerWhenReady()
+    {
+        while (!Advertisement.IsReady(placementId))
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.Show(placementId);
+    }
+}
