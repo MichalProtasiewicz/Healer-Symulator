63301b923b4a9d6d4af2832975c9a20f90a2b37c
Prota <protez95@gmail.com>
2020-02-14 03:42:02 +0100

    Update BossInfoPanel.cs

--- Assets/Scripts/UI/BossInfoPanel.cs
+++ Assets/Scripts/UI/BossInfoPanel.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class BossInfoPanel : MonoBehaviour
 {
     public GameObject infoPanel;
+
+    public TextMeshProUGUI textBossName;
+    public Image iconDebuff1;
+    public TextMeshProUGUI textDebuff1;
+    public Image iconDebuff2;
+    public TextMeshProUGUI textDebuff2;
+    public TextMeshProUGUI textTips;
 
     void Start()
     {
