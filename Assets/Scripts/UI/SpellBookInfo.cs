3767cf3ed794496dfef11e94b7194ee619c91c61
Prota <protez95@gmail.com>
2020-02-17 18:33:39 +0100

    Update SpellBookInfo.cs

--- Assets/Scripts/UI/SpellBookInfo.cs
+++ Assets/Scripts/UI/SpellBookInfo.cs
@@ -23,26 +23,39 @@ public class SpellBookInfo : MonoBehaviour
             this.spellName = spellName;
             this.spellInfo = spellInfo;
         }
-    } 
+    }
+
+    public List<SpellInfo> SpellsInfo = new List<SpellInfo>();
 
     SpellInfo heal = new SpellInfo(0, "Heal", "Slow, efficient, single target direct heal.");
     SpellInfo flashHeal = new SpellInfo(1, "Flash Heal", "Fast, more expensive single target heal.");
     SpellInfo renew = new SpellInfo(2, "Renew", "Weak direct heal with a Heal over Time effect");
     SpellInfo dispelMagic = new SpellInfo(3, "Dispel Magic", "Allows you to remove negative debuffs from your ally.");
-    SpellInfo prayerOfHealing = new SpellInfo(4, "Prayer of Healing", "Costly proximity heal that heals the target group");
+    SpellInfo prayerOfHealing = new SpellInfo(4, "Prayer of Healing", "Costly proximity heal that heals the target group.");
     SpellInfo prayerOfMending = new SpellInfo(5, "Prayer of Mending", "A charge based heal that is cast on a friendly target which heals them when they take damage and then bounces to another friendly target if charges are remaining.");
     SpellInfo divineHymn = new SpellInfo(6, "Divine Hymn", "Large channeled raid cooldown that heals all raiders over time.");
-    SpellInfo holyWordSerenity = new SpellInfo(7, "Holy Word Serenity", "Strong, instant direct heal");
+    SpellInfo holyWordSerenity = new SpellInfo(7, "Holy Word Serenity", "Strong, instant direct heal.");
     SpellInfo holyWordSanctify = new SpellInfo(8, "Holy Word Sanctify", "Strong, instant heal that heals the target group.");
 
     void Start()
     {
-        
+        SpellsInfo.Add(heal);
+        SpellsInfo.Add(flashHeal);
+        SpellsInfo.Add(renew);
+        SpellsInfo.Add(dispelMagic);
+        SpellsInfo.Add(prayerOfHealing);
+        SpellsInfo.Add(prayerOfMending);
+        SpellsInfo.Add(divineHymn);
+        SpellsInfo.Add(holyWordSerenity);
+        SpellsInfo.Add(holyWordSanctify);
     }
 
     void Update()
     {
         
     }
+
+
+
 }
 
