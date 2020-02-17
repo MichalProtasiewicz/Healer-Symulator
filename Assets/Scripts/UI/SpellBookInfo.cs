a291d92801a5ea732a0ccac25d189fa8793bd5e9
Prota <protez95@gmail.com>
2020-02-17 23:44:04 +0100

    SpellBook data

--- Assets/Scripts/UI/SpellBookInfo.cs
+++ Assets/Scripts/UI/SpellBookInfo.cs
@@ -11,6 +11,8 @@ public class SpellBookInfo : MonoBehaviour
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI infoText;
 
+    public Sprite[] sprites; 
+
     public struct SpellInfo
     {
         public int spellId;
@@ -25,20 +27,21 @@ public class SpellBookInfo : MonoBehaviour
         }
     }
 
-    public List<SpellInfo> SpellsInfo = new List<SpellInfo>();
+    List<SpellInfo> SpellsInfo = new List<SpellInfo>();
 
-    SpellInfo heal = new SpellInfo(0, "Heal", "Slow, efficient, single target direct heal.");
-    SpellInfo flashHeal = new SpellInfo(1, "Flash Heal", "Fast, more expensive single target heal.");
-    SpellInfo renew = new SpellInfo(2, "Renew", "Weak direct heal with a Heal over Time effect");
-    SpellInfo dispelMagic = new SpellInfo(3, "Dispel Magic", "Allows you to remove negative debuffs from your ally.");
-    SpellInfo prayerOfHealing = new SpellInfo(4, "Prayer of Healing", "Costly proximity heal that heals the target group.");
-    SpellInfo prayerOfMending = new SpellInfo(5, "Prayer of Mending", "A charge based heal that is cast on a friendly target which heals them when they take damage and then bounces to another friendly target if charges are remaining.");
-    SpellInfo divineHymn = new SpellInfo(6, "Divine Hymn", "Large channeled raid cooldown that heals all raiders over time.");
-    SpellInfo holyWordSerenity = new SpellInfo(7, "Holy Word Serenity", "Strong, instant direct heal.");
-    SpellInfo holyWordSanctify = new SpellInfo(8, "Holy Word Sanctify", "Strong, instant heal that heals the target group.");
-
-    void Start()
+    void Awake()
     {
+        SpellInfo heal = new SpellInfo(0, "Heal", "Slow, efficient, single target direct heal.");
+        SpellInfo flashHeal = new SpellInfo(1, "Flash Heal", "Fast, more expensive single target heal.");
+        SpellInfo renew = new SpellInfo(2, "Renew", "Weak direct heal with a Heal over Time effect");
+        SpellInfo dispelMagic = new SpellInfo(3, "Dispel Magic", "Allows you to remove negative debuffs from your ally.");
+        SpellInfo prayerOfHealing = new SpellInfo(4, "Prayer of Healing", "Costly proximity heal that heals the target group.");
+        SpellInfo prayerOfMending = new SpellInfo(5, "Prayer of Mending", "A charge based heal that is cast on a friendly target which heals them when they take damage and then bounces to another friendly target if charges are remaining.");
+        SpellInfo divineHymn = new SpellInfo(6, "Divine Hymn", "Large channeled raid cooldown that heals all raiders over time.");
+        SpellInfo holyWordSerenity = new SpellInfo(7, "Holy Word Serenity", "Strong, instant direct heal.");
+        SpellInfo holyWordSanctify = new SpellInfo(8, "Holy Word Sanctify", "Strong, instant heal that heals the target group.");
+
+
         SpellsInfo.Add(heal);
         SpellsInfo.Add(flashHeal);
         SpellsInfo.Add(renew);
@@ -50,11 +53,24 @@ public class SpellBookInfo : MonoBehaviour
         SpellsInfo.Add(holyWordSanctify);
     }
 
-    void Update()
+    public void OpenSpellInfo(int spellId)
     {
-        
+        foreach(SpellInfo spellInfo in SpellsInfo)
+        {
+            if(spellInfo.spellId == spellId)
+            {
+                infoPanel.SetActive(true);
+                image.sprite = sprites[spellId];
+                nameText.text = spellInfo.spellName;
+                infoText.text = spellInfo.spellInfo;
+            }
+        }
     }
 
+    public void CloseSpellInfo()
+    {
+        infoPanel.SetActive(false);
+    }
 
 
 }
