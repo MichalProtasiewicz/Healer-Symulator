4855a0f75df9009ddbe8c6b7e9f9da31a1b24252
Prota <protez95@gmail.com>
2020-02-18 01:00:52 +0100

    text info spell update

--- Assets/Scripts/UI/SpellBookInfo.cs
+++ Assets/Scripts/UI/SpellBookInfo.cs
@@ -31,15 +31,15 @@ public class SpellBookInfo : MonoBehaviour
 
     void Awake()
     {
-        SpellInfo heal = new SpellInfo(0, "Heal", "Slow, efficient, single target direct heal.");
-        SpellInfo flashHeal = new SpellInfo(1, "Flash Heal", "Fast, more expensive single target heal.");
-        SpellInfo renew = new SpellInfo(2, "Renew", "Weak direct heal with a Heal over Time effect");
-        SpellInfo dispelMagic = new SpellInfo(3, "Dispel Magic", "Allows you to remove negative debuffs from your ally.");
-        SpellInfo prayerOfHealing = new SpellInfo(4, "Prayer of Healing", "Costly proximity heal that heals the target group.");
-        SpellInfo prayerOfMending = new SpellInfo(5, "Prayer of Mending", "A charge based heal that is cast on a friendly target which heals them when they take damage and then bounces to another friendly target if charges are remaining.");
-        SpellInfo divineHymn = new SpellInfo(6, "Divine Hymn", "Large channeled raid cooldown that heals all raiders over time.");
-        SpellInfo holyWordSerenity = new SpellInfo(7, "Holy Word Serenity", "Strong, instant direct heal.");
-        SpellInfo holyWordSanctify = new SpellInfo(8, "Holy Word Sanctify", "Strong, instant heal that heals the target group.");
+        SpellInfo heal = new SpellInfo(0, "Heal", "Simple single target heal with a long cast time. It is generally used to heal targets when they are not taking heavy damage and when they are not in immediate danger.");
+        SpellInfo flashHeal = new SpellInfo(1, "Flash Heal", "Simple single target heal with a short cast time. It is useful in more pressing situations, where the target is in danger of dying.");
+        SpellInfo renew = new SpellInfo(2, "Renew", "Heals the target over 15 seconds. It is generally used to heal tanks or constantly low damage.");
+        SpellInfo dispelMagic = new SpellInfo(3, "Dispel Magic", "Allows you to remove harmful magic effects from your ally.");
+        SpellInfo prayerOfHealing = new SpellInfo(4, "Prayer of Healing", "Heals the target group. It has a cast time, and it is one of your most reliable sources of AoE healing.");
+        SpellInfo prayerOfMending = new SpellInfo(5, "Prayer of Mending", "Places a buff on the targeted ally, which heals them when they take damage and jumps to another ally (up to 5 times). This is one of your most efficient spells.");
+        SpellInfo divineHymn = new SpellInfo(6, "Divine Hymn", "Channeled spell that heals all raid members. It is a powerful raid cooldown that can be used to mitigate high raid damage.");
+        SpellInfo holyWordSerenity = new SpellInfo(7, "Holy Word Serenity", "Instant cast, single-target heal with a 60-second cooldown that can be further reduced by casting Heal or Flash Heal.");
+        SpellInfo holyWordSanctify = new SpellInfo(8, "Holy Word Sanctify", "Instant cast spell that heals target group. It has a 60-second cooldown, which can be further reduced by casting Prayer of Healing or Renew.");
 
 
         SpellsInfo.Add(heal);
