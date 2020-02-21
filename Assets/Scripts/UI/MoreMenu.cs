535c12e6f4956606915ab8f71301a2b28217462f
Prota <protez95@gmail.com>
2020-02-21 23:22:13 +0100

    MoreMenuImplementation

---
+++ Assets/Scripts/UI/MoreMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MoreMenu : MonoBehaviour
+{
+    public GameObject moreMenuPanel;
+    public bool isOppened;
+    public TextMeshProUGUI buttonText;
+    public Image buttonImage;
+
+
+    private void Start()
+    {
+        isOppened = false;
+        moreMenuPanel.SetActive(false);
+        buttonImage.color = new Color32(255,255,255,255);
+        buttonText.color = new Color32(255, 255, 255, 255);
+    }
+
+    public void OpenCloseMenu()
+    {
+        if(isOppened)
+        {
+            isOppened = false;
+            moreMenuPanel.SetActive(false);
+            buttonImage.color = new Color32(255, 255, 255, 255);
+            buttonText.color = new Color32(255, 255, 255, 255);
+        }
+        else
+        {
+            isOppened = true;
+            moreMenuPanel.SetActive(true);
+            buttonImage.color = new Color32(255, 247, 0, 255);
+            buttonText.color = new Color32(255, 247, 0, 255);
+        }
+    }
+}
