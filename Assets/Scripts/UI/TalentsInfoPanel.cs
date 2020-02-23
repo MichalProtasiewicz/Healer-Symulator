676ba4857dc76dba8334ad85380ea0d144c5288a
Prota <protez95@gmail.com>
2020-02-23 21:10:56 +0100

    implementation resetProgress

--- Assets/Scripts/UI/TalentsInfoPanel.cs
+++ Assets/Scripts/UI/TalentsInfoPanel.cs
@@ -35,21 +35,21 @@ public class TalentsInfoPanel : MonoBehaviour
 
     void Awake()
     {
-        TalentInfo talent1 = new TalentInfo(0, "Spirit Enchance", "Light bless you, your mana regenerate faster.");
-        TalentInfo talent2 = new TalentInfo(1, "Inteligence Power", "Long time meditation give you more inteligence");
-        TalentInfo talent3 = new TalentInfo(2, "Prayer of Fortidue", "Powerfull stamina buff for all raid members.");
-        TalentInfo talent4 = new TalentInfo(3, "Previous focus", "Heal and Flash Heal, heal previeous target for small amout");
-        TalentInfo talent5 = new TalentInfo(4, "Renew renew", "Heal and Flash Heal can refresh renew on target.");
-        TalentInfo talent6 = new TalentInfo(5, "Bonus Flash", "Cast Flash Heal have chance to make your next Flash Heal instant");
-        TalentInfo talent7 = new TalentInfo(6, "Ressurect", "After Angel you not die.");
-        TalentInfo talent8 = new TalentInfo(7, "Angel Bless", "Your angel form takes more time.");
-        TalentInfo talent9 = new TalentInfo(8, "Revive", "You can ressurect first die person in your raid.");
-        TalentInfo talent10 = new TalentInfo(9, "Prayer of Mana", "Prayer of Healing cost less mana.");
-        TalentInfo talent11 = new TalentInfo(10, "Dispel Healing", "Dispel Magic also heal target.");
-        TalentInfo talent12 = new TalentInfo(11, "Bless Hymn", "Untill Divine Hymn, Prayer of Mending jump to others members, without lose stacks.");
-        TalentInfo talent13 = new TalentInfo(12, "Speed Holy Word", "Holy Word Serenity and Sanctify decrease cooldown more efficiently.");
-        TalentInfo talent14 = new TalentInfo(13, "Holy Word Power", "When Holy Word Serenity or Sanctify finish cooldown, you will heal all raid for small amout.");
-        TalentInfo talent15 = new TalentInfo(14, "Prayer over Time", "Prayer of Mending have chance to leave Renew.");
+        TalentInfo talent1 = new TalentInfo(1, "Spirit Enchance", "Light bless you, your mana regenerate faster.");
+        TalentInfo talent2 = new TalentInfo(2, "Inteligence Power", "Long time meditation give you more inteligence");
+        TalentInfo talent3 = new TalentInfo(3, "Prayer of Fortidue", "Powerfull stamina buff for all raid members.");
+        TalentInfo talent4 = new TalentInfo(4, "Previous focus", "Heal and Flash Heal, heal previeous target for small amout");
+        TalentInfo talent5 = new TalentInfo(5, "Renew renew", "Heal and Flash Heal can refresh renew on target.");
+        TalentInfo talent6 = new TalentInfo(6, "Bonus Flash", "Cast Flash Heal have chance to make your next Flash Heal instant");
+        TalentInfo talent7 = new TalentInfo(7, "Ressurect", "After Angel you not die.");
+        TalentInfo talent8 = new TalentInfo(8, "Angel Bless", "Your angel form takes more time.");
+        TalentInfo talent9 = new TalentInfo(9, "Revive", "You can ressurect first die person in your raid.");
+        TalentInfo talent10 = new TalentInfo(10, "Prayer of Mana", "Prayer of Healing cost less mana.");
+        TalentInfo talent11 = new TalentInfo(11, "Dispel Healing", "Dispel Magic also heal target.");
+        TalentInfo talent12 = new TalentInfo(12, "Bless Hymn", "Untill Divine Hymn, Prayer of Mending jump to others members, without lose stacks.");
+        TalentInfo talent13 = new TalentInfo(13, "Speed Holy Word", "Holy Word Serenity and Sanctify decrease cooldown more efficiently.");
+        TalentInfo talent14 = new TalentInfo(14, "Holy Word Power", "When Holy Word Serenity or Sanctify finish cooldown, you will heal all raid for small amout.");
+        TalentInfo talent15 = new TalentInfo(15, "Prayer over Time", "Prayer of Mending have chance to leave Renew.");
 
         TalentsInfo.Add(talent1);
         TalentsInfo.Add(talent2);
@@ -76,7 +76,7 @@ public class TalentsInfoPanel : MonoBehaviour
             {
                 actualOpenTalentId = talentId;
                 infoPanel.SetActive(true);
-                image.sprite = sprites[talentId];
+                image.sprite = sprites[talentId-1];
                 nameText.text = talentInfo.talentName;
                 infoText.text = talentInfo.talentInfo;
             }
