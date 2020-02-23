cb64f12db79c0a976c6684d578411f874d40afe0
Prota <protez95@gmail.com>
2020-02-23 17:28:41 +0100

    Update BannerAdScript.cs

--- Assets/Scripts/Monetization/BannerAdScript.cs
+++ Assets/Scripts/Monetization/BannerAdScript.cs
@@ -22,5 +22,7 @@ public class BannerAdScript : MonoBehaviour
             yield return new WaitForSeconds(0.5f);
         }
         Advertisement.Banner.Show(placementId);
+        yield return new WaitForSeconds(15.0f);
+        Advertisement.Banner.Hide();
     }
 }
