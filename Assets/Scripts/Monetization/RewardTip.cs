9ac06aeb7954097057b16b21b548b82d1922b0d5
Prota <protez95@gmail.com>
2020-02-26 23:41:31 +0100

    Ads tips

---
+++ Assets/Scripts/Monetization/RewardTip.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Button))]
+public class RewardTip : MonoBehaviour, IUnityAdsListener
+{
+
+#if UNITY_IOS
+    private string gameId = "3360461";
+#elif UNITY_ANDROID
+    private string gameId = "3360460";
+#endif
+
+    public Button myButton;
+    bool givedReward;
+    public string myPlacementId = "rewardedVideo";
+
+    public GameObject tipText;
+    public GameObject tipButton;
+
+    void Start()
+    {
+        tipText.SetActive(false);
+        tipButton.SetActive(true);
+
+
+        // Set interactivity to be dependent on the Placement’s status:
+        myButton.interactable = Advertisement.IsReady(myPlacementId);
+
+        // Map the ShowRewardedVideo function to the button’s click listener:
+        if (myButton) myButton.onClick.AddListener(ShowRewardedVideo);
+
+        // Initialize the Ads listener and service:
+        Advertisement.AddListener(this);
+        Advertisement.Initialize(gameId, true);
+    }
+
+    // Implement a function for showing a rewarded video ad:
+    void ShowRewardedVideo()
+    {
+        Advertisement.Show(myPlacementId);
+    }
+
+    // Implement IUnityAdsListener interface methods:
+    public void OnUnityAdsReady(string placementId)
+    {
+        // If the ready Placement is rewarded, activate the button: 
+        if (placementId == myPlacementId)
+        {
+            myButton.interactable = true;
+        }
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+
+        // Define conditional logic for each ad completion status:
+        if (showResult == ShowResult.Finished)
+        {
+            // Reward the user for watching the ad to completion.
+            TipsVisible();
+        }
+        else if (showResult == ShowResult.Skipped)
+        {
+            // Do not reward the user for skipping the ad.
+        }
+        else if (showResult == ShowResult.Failed)
+        {
+            Debug.LogWarning("The ad did not finish due to an error.");
+        }
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+        // Log the error.
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        // Optional actions to take when the end-users triggers an ad.
+        givedReward = false;
+    }
+
+    public void TipsVisible()
+    {
+        tipButton.SetActive(false);
+        tipText.SetActive(true);
+    }
+
+
+}
--- Assets/Scripts/UI/BossInfoPanel.cs
+++ Assets/Scripts/UI/BossInfoPanel.cs
@@ -92,4 +92,5 @@ public class BossInfoPanel : MonoBehaviour
     {
         SceneManager.LoadScene(sceneToChangeName);
     }
+
 }
