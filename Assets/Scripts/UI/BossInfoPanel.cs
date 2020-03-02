c4dfe055631e3b5101d94f55f2e24cf453380cb8
Prota <protez95@gmail.com>
2020-03-02 20:43:40 +0100

    bossIndicators

--- Assets/Scripts/UI/BossInfoPanel.cs
+++ Assets/Scripts/UI/BossInfoPanel.cs
@@ -35,10 +35,61 @@ public class BossInfoPanel : MonoBehaviour
     void Awake()
     {
         //Boss id co 2, bo dwie ikony debufu
-        BossInfo defaultBoss = new BossInfo(0, "Default Boss", "You need to dispel it.", "", "Heal better, thats all.", "BossScene");
-        BossInfo arthas = new BossInfo(2, "Arthas", "Defile on me.", "Dispel it", "Use renew on tanks", "copyBoss");
-        BossesInfo.Add(defaultBoss);
-        BossesInfo.Add(arthas);
+        BossInfo boss1 = new BossInfo(0, "Wolf", "", "", "If you lose better go afk.", "Boss1");
+        BossInfo boss2 = new BossInfo(2, "Goblin", "", "", "Flash Heal - your main single target heal.", "Boss2");
+        BossInfo boss3 = new BossInfo(4, "Troll", "", "", "Use renew on tanks.", "Boss3");
+        BossInfo boss4 = new BossInfo(6, "Gnoll", "", "", "You dont need heal all time, try safe your mana.", "Boss3.1");
+        BossInfo boss5 = new BossInfo(8, "Harpie", "", "", "Heal have higher cast time than Flash Heal, but is more effective.", "Boss4");
+        BossInfo boss6 = new BossInfo(10, "Harpie Queen", "", "", "Prayer of Healing is your main AoE heal.", "Boss5");
+        BossInfo boss7 = new BossInfo(12, "Murlocs", "", "", "Tanks have a lot of stamina and armor, their main aim is keep agro.", "Boss5.1");
+        BossInfo boss8 = new BossInfo(14, "Murlocs", "", "", "Healer and Dps get more damage.", "Boss5.2");
+        BossInfo boss9 = new BossInfo(16, "Murloc King", "", "", "Healers in your group help you heal raid.", "Boss5.3");
+
+        ///DEBUFY
+        BossInfo boss10 = new BossInfo(18, "Assasin Troll", "", "", "If you are pro heal and previous challenges was easy, you can try kill true raid boss for pass tutorial.", "Boss6");
+        BossInfo boss11 = new BossInfo(20, "Hunter Troll", "", "", ".......", "Boss6.1");
+        BossInfo boss12 = new BossInfo(22, "Berserker Troll", "", "", ".......", "Boss6.2");
+        BossInfo boss13 = new BossInfo(24, "Vodo Troll", "", "", ".......", "Boss6.3");
+        BossInfo boss14 = new BossInfo(26, "Ranger Troll", "", "", ".......", "Boss6.4");
+        BossInfo boss15 = new BossInfo(28, "King Troll", "", "", ".......", "Boss6.5");
+        BossInfo boss16 = new BossInfo(30, "Hunter Gnoll", "", "", ".......", "Boss7");
+        BossInfo boss17 = new BossInfo(32, "Rogue Gnoll", "", "", ".......", "Boss7.1");
+        BossInfo boss18 = new BossInfo(34, "Assasin Gnoll", "", "", ".......", "Boss7.2");
+        BossInfo boss19 = new BossInfo(30, "Kobold", "", "", ".......", "Boss7.3");
+        BossInfo boss20 = new BossInfo(32, "Skeleton", "", "", ".......", "Boss8");
+        BossInfo boss21 = new BossInfo(34, "Zombie", "", "", ".......", "Boss8.1");
+        BossInfo boss22 = new BossInfo(36, "Ghost", "", "", ".......", "Boss8.2");
+        BossInfo boss23 = new BossInfo(38, "Abonimation", "", "", ".......", "Boss8.3");
+        BossInfo boss24 = new BossInfo(40, "Vampir", "", "", ".......", "Boss8.4");
+        BossInfo boss25 = new BossInfo(42, "Lich", "", "", ".......", "Boss8.5");
+        BossInfo boss26 = new BossInfo(32, "Nathrezim", "", "", ".......", "Boss9");
+
+        BossesInfo.Add(boss1);
+        BossesInfo.Add(boss2);
+        BossesInfo.Add(boss3);
+        BossesInfo.Add(boss4);
+        BossesInfo.Add(boss5);
+        BossesInfo.Add(boss6);
+        BossesInfo.Add(boss7);
+        BossesInfo.Add(boss8);
+        BossesInfo.Add(boss9);
+        BossesInfo.Add(boss10);
+        BossesInfo.Add(boss11);
+        BossesInfo.Add(boss12);
+        BossesInfo.Add(boss13);
+        BossesInfo.Add(boss14);
+        BossesInfo.Add(boss15);
+        BossesInfo.Add(boss16);
+        BossesInfo.Add(boss17);
+        BossesInfo.Add(boss18);
+        BossesInfo.Add(boss19);
+        BossesInfo.Add(boss20);
+        BossesInfo.Add(boss21);
+        BossesInfo.Add(boss22);
+        BossesInfo.Add(boss23);
+        BossesInfo.Add(boss24);
+        BossesInfo.Add(boss25);
+        BossesInfo.Add(boss26);
     }
 
     public void OpenBossInfo(int bossId)
