75019c4b08af947df72cd2f5a54cf9437e78f335
Prota <protez95@gmail.com>
2020-03-06 23:03:12 +0100

    fix lasts bugs

--- Assets/Scripts/UI/BossInfoPanel.cs
+++ Assets/Scripts/UI/BossInfoPanel.cs
@@ -35,32 +35,32 @@ public class BossInfoPanel : MonoBehaviour
     void Awake()
     {
         //Boss id co 2, bo dwie ikony debufu
-        BossInfo boss1 = new BossInfo(0, "Wolf", "", "", "If you lose better go afk.", "Boss1");
+        BossInfo boss1 = new BossInfo(0, "Wolf", "", "", "If you lose, better go afk for this fight.", "Boss1");
         BossInfo boss2 = new BossInfo(2, "Goblin", "", "", "Flash Heal - your main single target heal.", "Boss2");
         BossInfo boss3 = new BossInfo(4, "Troll", "", "", "Use renew on tanks.", "Boss3");
         BossInfo boss4 = new BossInfo(6, "Gnoll", "", "", "You dont need heal all time, try safe your mana.", "Boss3.1");
         BossInfo boss5 = new BossInfo(8, "Harpie", "", "", "Heal have higher cast time than Flash Heal, but is more effective.", "Boss4");
         BossInfo boss6 = new BossInfo(10, "Harpie Queen", "", "", "Prayer of Healing is your main AoE heal.", "Boss5");
         BossInfo boss7 = new BossInfo(12, "Murlocs", "", "", "Tanks have a lot of stamina and armor, their main aim is keep agro.", "Boss5.1");
-        BossInfo boss8 = new BossInfo(14, "Murlocs", "", "", "Healer and Dps get more damage.", "Boss5.2");
+        BossInfo boss8 = new BossInfo(14, "Murlocs", "", "", "Healer and Dps get more damage than tanks.", "Boss5.2");
         BossInfo boss9 = new BossInfo(16, "Murloc King", "", "", "Healers in your group help you heal raid.", "Boss5.3");
-        BossInfo boss10 = new BossInfo(18, "Assasin Troll", "Bleed - deal damage over time.", "", "If you are pro heal and previous challenges was easy, you can try kill true raid boss for pass tutorial.", "Boss6");
-        BossInfo boss11 = new BossInfo(20, "Hunter Troll", "Poison - deal damage over time.", "", ".......", "Boss6.1");
-        BossInfo boss12 = new BossInfo(22, "Berserker Troll", "", "", ".......", "Boss6.2");
-        BossInfo boss13 = new BossInfo(24, "Vodo Troll", "Dark Bomb - deal huge damage if not dispeled.", "", ".......", "Boss6.3");
-        BossInfo boss14 = new BossInfo(26, "Ranger Troll", "Poison - deal damage over time.", "", ".......", "Boss6.4");
-        BossInfo boss15 = new BossInfo(28, "King Troll", "Endless Power - give awesome buff to stats.", "", ".......", "Boss6.5");
-        BossInfo boss16 = new BossInfo(30, "Hunter Gnoll", "", "", ".......", "Boss7");
-        BossInfo boss17 = new BossInfo(32, "Rogue Gnoll", "Toxic - deal damage over time.", "", ".......", "Boss7.1");
-        BossInfo boss18 = new BossInfo(34, "Assasin Gnoll", "Bleed - deal damage over time.", "", ".......", "Boss7.2");
-        BossInfo boss19 = new BossInfo(36, "Kobold", "CandleFire - deal huge damage if not dispelled.", "", ".......", "Boss7.3");
-        BossInfo boss20 = new BossInfo(38, "Skeleton", "", "", ".......", "Boss8");
-        BossInfo boss21 = new BossInfo(40, "Zombie", "", "", ".......", "Boss8.1");
-        BossInfo boss22 = new BossInfo(42, "Ghost", "", "", ".......", "Boss8.2");
-        BossInfo boss23 = new BossInfo(44, "Abonimation", "Plague - deal damage over time.", "", ".......", "Boss8.3");
-        BossInfo boss24 = new BossInfo(46, "Vampir", "Bleed - deal damage over time.", "", ".......", "Boss8.4");
-        BossInfo boss25 = new BossInfo(48, "Lich", "Frozen Tomb - kill target if not dispelled.", "", ".......", "Boss8.5");
-        BossInfo boss26 = new BossInfo(50, "Nathrezim", "Plague - deal damage over time.", "Mind Blast - kill target if not dispelled.", ".......", "Boss9");
+        BossInfo boss10 = new BossInfo(18, "Assasin Troll", "Bleed - deal damage over time.", "", "Try to keep alive tanks, you need them.", "Boss6");
+        BossInfo boss11 = new BossInfo(20, "Hunter Troll", "Poison - deal damage over time.", "", "You need to learn your spells for better heal your group.", "Boss6.1");
+        BossInfo boss12 = new BossInfo(22, "Berserker Troll", "", "", "Before start fight, read boss abilites, not all debuffs you need to dispel.", "Boss6.2");
+        BossInfo boss13 = new BossInfo(24, "Vodo Troll", "Dark Bomb - deal huge damage if not dispeled.", "", "Don't wait too long with your highest cooldown spless, better use it multiple times.", "Boss6.3");
+        BossInfo boss14 = new BossInfo(26, "Ranger Troll", "Poison - deal damage over time.", "", "Try to not overheall raid.", "Boss6.4");
+        BossInfo boss15 = new BossInfo(28, "King Troll", "Endless Power - give awesome buff to stats.", "", "Use renew on tanks.", "Boss6.5");
+        BossInfo boss16 = new BossInfo(30, "Hunter Gnoll", "", "", "You dont need heal all time, try safe your mana.", "Boss7");
+        BossInfo boss17 = new BossInfo(32, "Rogue Gnoll", "Toxic - deal damage over time.", "", "Flash Heal - your main single target heal.", "Boss7.1");
+        BossInfo boss18 = new BossInfo(34, "Assasin Gnoll", "Bleed - deal damage over time.", "", "Prayer of Healing is your main AoE heal.", "Boss7.2");
+        BossInfo boss19 = new BossInfo(36, "Kobold", "CandleFire - deal huge damage if not dispelled.", "", "Healers in your group help you heal raid.", "Boss7.3");
+        BossInfo boss20 = new BossInfo(38, "Skeleton", "", "", "Tanks have a lot of stamina and armor, their main aim is keep agro.", "Boss8");
+        BossInfo boss21 = new BossInfo(40, "Zombie", "", "", "Healer and Dps get more damage than tanks.", "Boss8.1");
+        BossInfo boss22 = new BossInfo(42, "Ghost", "", "", "Try to keep alive tanks, you need them.", "Boss8.2");
+        BossInfo boss23 = new BossInfo(44, "Abonimation", "Plague - deal damage over time.", "", "Try to not overheall raid.", "Boss8.3");
+        BossInfo boss24 = new BossInfo(46, "Vampir", "Bleed - deal damage over time.", "", "Before start fight, read boss abilites.", "Boss8.4");
+        BossInfo boss25 = new BossInfo(48, "Lich", "Frozen Tomb - kill target if not dispelled.", "", "Don't wait too long with your highest cooldown spless, better use it multiple times.", "Boss8.5");
+        BossInfo boss26 = new BossInfo(50, "Nathrezim", "Plague - deal damage over time.", "Mind Blast - kill target if not dispelled.", "Use holy Words on cooldown.", "Boss9");
 
         BossesInfo.Add(boss1);
         BossesInfo.Add(boss2);
